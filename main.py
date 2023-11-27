ba4d83beccb0d91cb3f9eab9422b130a864a0de8
Pedro Roque <pedrohsroque@gmail.com>
2023-11-27 12:28:45 -0300

    edit main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from unidecode import unidecode
-from src.requests_extractor import RequestsExtractor
-from src.loader import Loader
+from extractors.requests_extractor import RequestsExtractor
+from loaders.loader import Loader
 from config.config import db_credentials
 
-championships_list = [
-    "Campeonato Brasileiro Série A 2023",
-    "Campeonato Brasileiro Série B 2023",
-]
+championships_list = []
+
+extract_a = input("Extrair dados Série A? S/n ")
+if extract_a == "S":
+    championships_list.append("Campeonato Brasileiro Série A 2023")
+
+extract_b = input("Extrair dados Série B? S/n ")
+if extract_b == "S":
+    championships_list.append("Campeonato Brasileiro Série B 2023")
 
 extractor = RequestsExtractor()
 for championship in championships_list:
@@ -14,12 +19,14 @@ for championship in championships_list:
     championship_data = extractor.get_championship_data(championship)
     extractor.save_data(championship_data, championship)
 
-loader = Loader(db_credentials)
-files = [f"{championship}" for championship in championships_list]
-for file in files:
-    full_path = f"data/{file}.csv"
-    table_name = unidecode(file.replace('-','_').replace(' ','_').lower())
-    loader.create_table(full_path, table_name)
-    loader.delete_from_table(table_name)
-    loader.insert_data_from_csv(full_path, table_name)
-    loader.connection.commit()
+load = input("Carregar dados? S/n ")
+if load == "S":
+    loader = Loader(db_credentials)
+    files = [f"{championship}" for championship in championships_list]
+    for file in files:
+        full_path = f"data/{file}.csv"
+        table_name = unidecode(file.replace('-','_').replace(' ','_').lower())
+        loader.create_table(full_path, table_name)
+        loader.delete_from_table(table_name)
+        loader.insert_data_from_csv(full_path, table_name)
+        loader.connection.commit()
